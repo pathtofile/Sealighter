58d3ab10597ad76e572676029319443a24622f0d
pathtofile <path.to.file@gmail.com>
2020-02-25 20:28:02 +1100

    updated reference to krabsETW fix to not have to make fake file outside codebase

--- Setup/fixNugetSubmodule.bat
+++ Setup/fixNugetSubmodule.bat
@@ -3,10 +3,9 @@ REM We pass in the full path to SolutionDir on the commandline
 SET SolutionDir=%1
 SET SourceDir=%SolutionDir%packages
 SET DestDir=%SolutionDir%krabsetw\krabs\packages
-SET FAKEFILE=%SolutionDir%..\35MSSharedLib1024.snk
 
 ECHO "Linking Solution Nuget Package Dir to Where KrabsETW expects it to be"
 ECHO "This is to fix how Nuget (fails to) work with git Submodules"
 if not exist %SourceDir% mkdir %SourceDir%
-if not exist %DestDir% mklink /j %DestDir% %SourceDir%
-ECHO "" > %FAKEFILE%
+if exist %DestDir% rmdir %DestDir%
+mklink /j %DestDir% %SourceDir%
